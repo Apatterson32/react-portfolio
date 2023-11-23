65e64712989ff3fbc1cf169ac6bf9aa0aadcce13
Alissa <pattersonal10@gmail.com>
2023-11-23 13:59:56 -0600

    adding and testig styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './assets/styles/global.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
