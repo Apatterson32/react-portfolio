c4a9ee6a2ea652a466f121a6b5329c0c87cf56d3
Alissa <pattersonal10@gmail.com>
2023-11-22 21:37:17 -0600

    updated aboutme page

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <section>
+        <h2>About Me</h2>
+        <p>
+          Hello! I'm Alissa Patterson, a passionate web developer with a strong
+          background in creating modern, responsive, and user-friendly web
+          applications.
+        </p>
+        <p>
+          I have continued to be driven in my pursuit of creating something that moves humankind forward, therefore I started my journey into programming in 2023. I love to code 
+          and explore new technologies to create efficient and scalable solutions.
+        </p>
+      </section>
+      <section>
+        <h3>Skills</h3>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript (ES6+)</li>
+          <li>React.js</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MongoDB</li>
+          <li>MySQL</li>
+          <li>GraphQL</li>
+          <li>RESTful APIs</li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default AboutMe;
