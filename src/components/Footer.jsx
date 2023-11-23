7ea2b636b3288cc30d7998fd9cb388cae10000c4
Alissa <pattersonal10@gmail.com>
2023-11-22 22:00:11 -0600

    added component basics and upadted app.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/Apatterson32" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/alissa-patterson-453864285/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
