c8e73b8152f91837ad850f3731154181371c80e3
Alissa <pattersonal10@gmail.com>
2023-11-24 15:13:11 -0600

    idkkk

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {},
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-  },
-});
+})
