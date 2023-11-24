fd13312e3c77e29da076c086c862135268838142
Alissa <pattersonal10@gmail.com>
2023-11-23 22:12:41 -0600

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className="body-content">
         <Header />
         <Routes>
           <Route path="/" element={<AboutMe />} />
