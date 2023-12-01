4f173a005cf4081f84055559b22ad02fd4f6cd6d
Alissa <pattersonal10@gmail.com>
2023-12-01 00:41:02 -0600

    updated portfolio images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,37 +4,31 @@ import '../assets/styles/portfolio.css'
 
 const projects = [
   {
-    title: 'Parks Planner',
     imageSrc: './src/assets/images/parksplanner.png',
     githubLink: 'https://github.com/Apatterson32/ParksPlanner',
   },
 
   {
-    title: 'Mind Mapper',
     imageSrc: './src/assets/images/mindmapper.png',
     githubLink: 'https://github.com/Apatterson32/Mind-Mapper',
   },
 
   {
-    title: 'Weather Report',
     imageSrc: './src/assets/images/weatherforecast.png',
     githubLink: 'https://github.com/Apatterson32/butterfly-wings',
   },
 
   {
-    title: 'Day Planner',
     imageSrc: './src/assets/images/dayplanner.png',
     githubLink: 'https://github.com/Apatterson32/human-time-planner',
   },
 
   {
-    title: 'Inventory Application',
     imageSrc: './src/assets/images/intellistock.png',
     githubLink: 'https://github.com/akidd15/IntelliStock',
   },
 
   {
-    title: 'Coding Quiz',
     imageSrc: './src/assets/images/codingquiz.png',
     githubLink: 'https://github.com/Apatterson32/timed-coding-quiz',
   },
